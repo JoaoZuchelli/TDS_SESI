d35b6f983540ae5feda93a8e1c4227b1755495cd
JoaoZuchelli <Zuchellijoaogabriel@gmail.com>
2026-03-05 12:10:59 -0300

    Operadores

---
+++ operadores/aritmeticos.js
@@ -0,0 +1,18 @@
+// valores literais
+console.log(10 + 5); // adiçao
+console.log(10 - 5); // subtraçao
+console.log(10 * 5); // multiplicaçao
+console.log(10 / 5); // divisao
+console.log(10 % 5); // resto da divisao
+
+// valores variaveis 
+
+let a = 100;
+let b = 50;
+
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
