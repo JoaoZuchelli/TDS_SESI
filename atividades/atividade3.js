5a17c26064d746f2dba4d2e904f2776b88fabef4
JoaoZuchelli <Zuchellijoaogabriel@gmail.com>
2026-03-27 11:17:38 -0300

    atividades

---
+++ atividades/atividade3.js
@@ -0,0 +1,12 @@
+var idade = 18
+var convite
+
+if(idade <=17 && convite == false ){
+    console.log("Ele nao pode entrar na festa");
+
+}else{
+    console.log(idade >=18 && convite == true);
+    console.log("Ele pode entrar na festa");
+
+}; 
+
